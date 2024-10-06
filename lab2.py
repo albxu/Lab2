7971e792ded42a898d544f214cec64f65550e058
Albert Xu <alt12xu@gmail.com>
2024-10-06 18:08:58 -0500

    implemented renamer algorithm

--- lab2.py
+++ lab2.py
@@ -1,12 +1,12 @@
 import sys
+import renamer
 
 def print_help():
     print("""
         Valid command-line arguments:
         -h                : Show this help message.
-        -s <name>         : prints tokens in token stream
-        -p <name>         : invokes parser and reports on success or failure
-        -r <name>         : prints human readable version of parser's IR
+        -x <name>         : scan and parse then input block, and then rename registers printing results to stdout
+        k <name>          : k is the number of registers available, name is the name of file with the input block
             """)
 
 def main():
@@ -17,8 +17,24 @@ def main():
         print_help()
         return
     
+    if argc == 2:
+        print('''ERROR: Not enough command line arguments found.
+              Try running with '-h' for help.''')
+        return
     
+    if argc == 3:
+        f_name = sys.argv[2]
 
+    try:
+        input_file = open(f_name,'r')
+    except:
+        print ("ERROR: Could not open file '"+f_name+"'. Exiting early.")
+        exit(0)
+
+    if flag == '-x':
+        renamer.rename(input_file)
+        return
+    
 if __name__ == "__main__":
 
     main()
--- renamer.py
+++ renamer.py
@@ -1,6 +1,61 @@
 # This file contains the renamer for the registers of the ILOC block.
 import sys
-from ..Lab1 import scanner, parser, linked_list
+from FrontEnd import parser
 
 
-def rename():
+def rename(file):
+    # Parse the input file to get the IR
+    result = parser.parse(file, True)
+    ir = result[0]
+    num_operations = result[1]
+    max_sr = result[2]
+
+    if ir is None:
+        return
+    
+    vr_name = 0
+    sr_to_vr = {}
+    lu = {}
+    # for i ← 0 to max SR number
+    # SRToVR[i] ← invalid
+    # LU[i] ← ∞
+    # index ← block length
+    for i in range(0, max_sr + 1):
+        sr_to_vr[i] = None
+        lu[i] = sys.maxsize
+    index = num_operations
+
+    # for each operation in the block in reverse order
+    current_node = ir.tail
+    while current_node.prev != ir.tail:
+        # for each def in the operation
+        def_operands = current_node.data.get_defs()
+        for def_operand in def_operands:
+            # if SRToVR[def] is invalid meaning unused def
+            operand_sr = def_operand.get_sr()
+            if sr_to_vr[operand_sr] is None:
+                sr_to_vr[operand_sr] = vr_name
+                vr_name += 1
+            def_operand.set_vr(sr_to_vr[operand_sr])
+
+            def_operand.set_nu(lu[operand_sr])
+            sr_to_vr[operand_sr] = None
+            lu[operand_sr] = sys.maxsize
+
+        use_operands = current_node.data.get_uses()
+        for use_operand in use_operands:
+            operand_sr = use_operand.get_sr()
+            if sr_to_vr[operand_sr] is None:
+                use_operand.set_vr(vr_name)
+                vr_name += 1
+            use_operand.set_vr(sr_to_vr[operand_sr])
+            use_operand.set_nu(lu[operand_sr])
+
+        for use_operand in use_operands:
+            lu[use_operand.get_sr()] = index
+        
+        index -= 1
+
+    ir.print_forward()
+
+
